cb844fb97d6ce0221c144abc5aeeb6a543da2270
nikhildahiyaa <nikkusmac@Nikhils-Mac.lan>
2025-08-26 15:05:41 -0700

    Update: fix send button + styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// src/main.jsx
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react"; // <- React entry
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Analytics />
+  </React.StrictMode>
+);
